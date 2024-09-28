c409be2a82c78d41f1745b2979d142e447dfb046
Ike <patrickikechukwukalukalu@gmail.com>
2024-09-28 16:14:25 +0100

    first commit

---
+++ src/route.jsx
@@ -0,0 +1,13 @@
+import SignUp from "../src/Components/SignUp";
+export const Route = [
+    {
+        path: '/',
+        element: <SignUp/>,
+        children: []
+    },
+    // {
+    //     path: '/welcome',
+    //     element: <Welcome/>,
+    //     children: []
+    // }
+]
